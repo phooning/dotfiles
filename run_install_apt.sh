6d0c5ad2672edcaffed8c80433e3145fbe277a99
David Pham <davidpham@utexas.edu>
2021-07-04 11:30:45 -0500

    Add apt makefiles

---
+++ run_install_apt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APT_PACKAGES=(
+  pulseeffects
+  lsp-plugins
+)
+
+echo "Initializing..."
+if [ $(sudo -n uptime 2>&1|grep "load"|wc -l) -eq 0 ]
+then
+  echo "We require a password in order to alter the system."
+  sudo -v
+  echo ""
+fi
+
+for package in "${APT_PACKAGES[@]}"
+do
+  echo
+  echo "Installing:" $package
+  echo
+  sudo apt install -y $package
+done
