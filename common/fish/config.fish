003a600ae015f4fb33453f543afb552bfe2ffa31
David Pham <ph0ne@protonmail.com>
2021-12-10 23:48:43 -0600

    Add M! python - conda

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -9,8 +9,16 @@ alias base='cd ~/toc && ls -lah'
 alias bt='set RUST_BACKTRACE 1 && echo "RUST_BACKTRACE=1"'
 alias ubt='set -e RUST_BACKTRACE && echo "Unset RUST_BACKTRACE"'
 alias map='nvim ~/toc/tacmap.txt'
+alias glcsv='/Users/david/toc/scripts/git-logs-to-csv.sh'
 
 fish_add_path /opt/homebrew/sbin
 
 # PostgreSQL for M1
 export LDFLAGS="-L/opt/homebrew/opt/libpq/lib"
+fish_add_path /opt/homebrew/opt/openjdk/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
