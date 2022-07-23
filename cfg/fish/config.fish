3eaa03038b06b379d83eac67df0d5594ada6a35e
David Pham <ph0ne@protonmail.com>
2022-07-23 13:58:56 -0500

    Cleanup

--- cfg/fish/config.fish
+++ cfg/fish/config.fish
@@ -4,12 +4,9 @@ alias ls=exa
 alias cat=ccat
 alias vi=vim
 alias v=nvim
-alias m='cd /Users/david/toc/inet/music && ls -lah'
-alias base='cd ~/toc && ls -lah'
 alias bt='set RUST_BACKTRACE 1 && echo "RUST_BACKTRACE=1"'
 alias ubt='set -e RUST_BACKTRACE && echo "Unset RUST_BACKTRACE"'
-alias map='nvim ~/toc/tacmap.txt'
-alias glcsv='/Users/david/toc/scripts/git-logs-to-csv.sh'
+alias l='exa -lahF --icons'
 
 fish_add_path /opt/homebrew/sbin
 
